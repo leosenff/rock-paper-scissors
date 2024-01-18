e3bee0370f257801d39c6f9ee1fc081e5cf3ccaa
Leonardo Senff <112514561+leosenff@users.noreply.github.com>
2024-01-18 17:41:26 -0300

    Remove old function and Add new funciotion to randomize 3 options

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-//Atribuir os números para cada opção
-function getComputerChoice (){
-    if (computerResponse === 1){
-        return str = "Rock";
-    } else if (computerResponse === 2){
-        return str = "Paper";
-    } else
-    return str = "Scissor";
+//Escolhe aleatoriamente uma das opções definidas
+function getComputerChoice() {
+    return arr[(Math.floor(Math.random() * arr.length))];
 }
 
-//Escolher um número aleatorio entre um min e max definido
-function getRandomInt (min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
+let arr = ["Rock", "Paper", "Scissor"];
+let computerChoice = getComputerChoice();
+
+console.log ("Computer choice is: ", computerChoice);
+
+if (computerChoice === "Rock") {
+    console.log("lol");
 }
-
-function getPlayerChoice (choice) {
-    
-    }
-
-
-let computerResponse = getRandomInt(1, 3);
 
 // console.log (computerResponse);
 // console.log (getComputerChoice());
 
-let choice = prompt("Escolha entre: \n -Rock \n -Paper \n -Scissor ");
-if (choice.toLowerCase() === "rock" ){
-    console.log("é pedra mesmo");
-} else if (choice.toLowerCase() === "paper") {
-    console.log("é papel mesmo");
-} else if (choice.toLowerCase() === "scissor") {
-    console.log ("é tesoura mesmo");
-} else 
-console.log("Valor não aceito");
+// let choice = prompt("Escolha entre: \n -Rock \n -Paper \n -Scissor ");
 
-console.log(choice);
+
+// if (choice.toLowerCase() === "rock" ){
+//     console.log("é pedra mesmo");
+// } else if (choice.toLowerCase() === "paper") {
+//     console.log("é papel mesmo");
+// } else if (choice.toLowerCase() === "scissor") {
+//     console.log ("é tesoura mesmo");
+// } else 
+// console.log("Valor não aceito");
+// //reiniciar
+
+// console.log(getPlayerChoice(choice));
+
+
+// console.log(choice);
 // console.log (getPlayerChoice());
