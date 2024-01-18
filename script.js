7a902b6c104d06aab09434f3c4495f63bde96e2d
Leonardo Senff <112514561+leosenff@users.noreply.github.com>
2024-01-18 16:56:46 -0300

    Add ignoring case sensitive

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Atribuir os números para cada opção
 function getComputerChoice (){
     if (computerResponse === 1){
         return str = "Rock";
@@ -7,12 +8,30 @@ function getComputerChoice (){
     return str = "Scissor";
 }
 
+//Escolher um número aleatorio entre um min e max definido
 function getRandomInt (min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+function getPlayerChoice (choice) {
+    
+    }
+
+
 let computerResponse = getRandomInt(1, 3);
 
+// console.log (computerResponse);
+// console.log (getComputerChoice());
 
-console.log (computerResponse);
-console.log (getComputerChoice());
+let choice = prompt("Escolha entre: \n -Rock \n -Paper \n -Scissor ");
+if (choice.toLowerCase() === "rock" ){
+    console.log("é pedra mesmo");
+} else if (choice.toLowerCase() === "paper") {
+    console.log("é papel mesmo");
+} else if (choice.toLowerCase() === "scissor") {
+    console.log ("é tesoura mesmo");
+} else 
+console.log("Valor não aceito");
+
+console.log(choice);
+// console.log (getPlayerChoice());
