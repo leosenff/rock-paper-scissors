3e59d0b1586b95edf88e38334987de132bf49f2f
Leonardo Senff <112514561+leosenff@users.noreply.github.com>
2024-01-22 15:43:13 -0300

    Add playRound function and remove other function

--- script.js
+++ script.js
@@ -3,33 +3,32 @@ function getComputerChoice() {
     return arr[(Math.floor(Math.random() * arr.length))];
 }
 
-let arr = ["Rock", "Paper", "Scissor"];
-let computerChoice = getComputerChoice();
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice.toLowerCase() == "rock" && computerChoice == "scissor") {
+        return "You Win! Rock beats Scissor";
+    } else if (playerChoice.toLowerCase() == "rock" && computerChoice == "paper"){
+        return "You Lose! Paper beats Rock"; 
 
-console.log ("Computer choice is: ", computerChoice);
+    } else if (playerChoice.toLowerCase() == "paper" && computerChoice == "rock"){
+        return "You Win! Paper beats Rock";
+    } else if (playerChoice.toLowerCase() == "paper" && computerChoice == "scissor"){
+        return "You Lose! Scissor beats Paper";
 
-if (computerChoice === "Rock") {
-    console.log("lol");
+    } else if (playerChoice.toLowerCase() == "scissor" && computerChoice == "paper"){
+        return "You win! Scissor beats paper";
+    } else if (playerChoice.toLowerCase() == "scissor" && computerChoice == "rock"){
+        return "You lose! Rock beats scissor";
+
+    } else 
+    return "It's a Tie";
 }
 
-// console.log (computerResponse);
-// console.log (getComputerChoice());
-
-// let choice = prompt("Escolha entre: \n -Rock \n -Paper \n -Scissor ");
+let arr = ["rock", "paper", "scissor"];
+let computerChoice = getComputerChoice();
 
 
-// if (choice.toLowerCase() === "rock" ){
-//     console.log("é pedra mesmo");
-// } else if (choice.toLowerCase() === "paper") {
-//     console.log("é papel mesmo");
-// } else if (choice.toLowerCase() === "scissor") {
-//     console.log ("é tesoura mesmo");
-// } else 
-// console.log("Valor não aceito");
-// //reiniciar
 
-// console.log(getPlayerChoice(choice));
-
-
-// console.log(choice);
-// console.log (getPlayerChoice());
+let playerChoice = "paper";
+console.log(playerChoice);
+console.log(computerChoice);
+console.log(playRound(playerChoice, computerChoice));
